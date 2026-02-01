73ac0580dbf24be5f7787276d7be5e212eb652b8
ViorelS9 <santiago.viorel9@gmail.com>
2026-02-01 09:10:12 +0100

    Tareas Hechas

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const addTaskBtn = document.getElementById("addTaskBtn");
-const taskInput = document.getElementById("taskInput");
-const taskList = document.getElementById("taskList");
-
 addTaskBtn.addEventListener("click", function () {
   const taskText = taskInput.value;
 
   if (taskText !== "") {
     const li = document.createElement("li");
     li.textContent = taskText;
+
+    li.addEventListener("click", function () {
+      li.style.textDecoration = "line-through";
+    });
+
     taskList.appendChild(li);
     taskInput.value = "";
   }
